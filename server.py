a3c4c73aab42283556ffa2269eac3f625a0ecad9
calebblas <calebblashuaroc@gmail.com>
2025-05-01 00:27:23 -0700

    Cambiar host y puerto para compatibilidad con Render

--- server.py
+++ server.py
@@ -73,4 +73,5 @@ def guardar_respuestas():
     return jsonify({"mensaje": "Respuestas guardadas correctamente."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
